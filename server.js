e4b2c2a1e141767eb0cdc630b120fc87cab45609
yessygc <yessgarciacampos@gmail.com>
2021-07-08 20:01:28 -0500

    agregando nodemon como dependencia secundaria y agregando rutas

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
+const notaRouter = express.Router();
+const categoriaRouter = express.Router();
+const adminRouter = express.Router();
 
-app.get('/home', (req, res) => {
-    res.send('Hello from express');
+//RUTAS PARA LAS NOTAS 
+notaRouter.get('/', (req, res) => {
+    res.send('Hello from nota router');
 });
+
+//RUTAS PARA LAS CATEGORIAS
+categoriaRouter.get('/', (req, res) => {
+    res.send('Hello from categoria router');
+});
+
+//RUTAS PARA EL ADMINISTRADOR
+adminRouter.get('/', (req, res) => {
+    res.send('Hello from admin router');
+});
+
+app.use('/nota', notaRouter);
+app.use('/categoria', categoriaRouter);
+app.use('/admin', adminRouter);
 
 app.listen(3003, () => {
     console.log('server listen on port', 3003)
